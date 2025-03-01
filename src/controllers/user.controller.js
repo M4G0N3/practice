abef23959d1a06cd2443e8bb872025a4a6336775
MagOne <aditya.magone@outlook.com>
2025-03-01 22:14:38 +0530

    daily push

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,20 @@ import { User } from "../models/user.models.js";
 import { uploadOnCLDNIARY } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessTokens();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, "SWW while generating refres and access token");
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   //get user details from frontend
   const { fullname, email, username, password } = req.body;
@@ -75,4 +89,67 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  //req body data le aao
+  const { email, username, password } = req.body;
+
+  if (!username || !email) {
+    throw new ApiError(400, "username or email is required");
+  }
+
+  //username or email
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  //find the user
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  //password check
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (isPasswordValid) {
+    throw new ApiError(401, "Invalid credentials");
+  }
+
+  //access and refresh token
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    -password - refreshToken
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  //send cookies
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged in Successfully"
+      )
+    );
+
+  const logoutUser = asyncHandler(async (req, res) => {
+    User.findById;
+  });
+
+  //send response -> registered successfully
+});
+
+export { registerUser, loginUser };
